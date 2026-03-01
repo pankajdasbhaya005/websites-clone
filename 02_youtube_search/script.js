7ea02614c0bb202c9d0db654506cb916675f14c0
pankaj dasbhaya <p9951542@gmail.com>
2026-03-01 22:43:20 +0530

    youtube search add

---
+++ 02_youtube_search/script.js
@@ -0,0 +1,15 @@
+function searchYouTube() {
+    const query = document.getElementById("searchInput").value;
+
+    if (query.trim() !== "") {
+        const url = "https://www.youtube.com/results?search_query=" + encodeURIComponent(query);
+        window.location.href = url;
+    }
+}
+
+document.getElementById("searchInput")
+.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        searchYouTube();
+    }
+});
